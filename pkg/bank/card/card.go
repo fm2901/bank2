a9512df4b1de5ade93cea1249822bfa5b864ca8d
muminf <fayzievmuminjon@gmail.com>
2021-09-15 22:16:00 +0500

    Init project

---
+++ pkg/bank/card/card.go
@@ -0,0 +1,26 @@
+package card
+
+import "bank/pkg/bank/types"
+
+func PaymentSources(cards []types.Card) []types.PaymentSource {
+	var result []types.PaymentSource
+
+	for _, card := range cards {
+
+		if !card.Active {
+			continue
+		}
+
+		if card.Balance < 1 {
+			continue
+		}
+
+		result = append(result, types.PaymentSource{
+			Type:    "Карта",
+			Number:  string(card.PAN),
+			Balance: card.Balance,
+		})
+	}
+
+	return result
+}
